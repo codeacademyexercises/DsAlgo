7be671000a7b9b60f26524994f15cec2f72a5ed8
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-01-25 22:56:03 +0530

    fixed some errors and added test cases

--- insertionSort.js
+++ insertionSort.js
@@ -7,92 +7,105 @@ for(let i=1;i<X.length;i++){
 
 X.slice(1,).forEach((x)=>{
     if(t>=h){
-        let i=t;
-        while(i>=h && Y[i]>x){
-            i-=1;
+        insertPosition=h;
+        while(insertPosition<=t && Y[insertPosition]<x){
+            insertPosition+=1;
         }
-        if(t-i>i+1){
-            if(i!=-1){
-            Y[X.length-1]=Y[0];
-            for(let j=1;j<=i;j++){
-                Y[j-1]=Y[j];
+        if(t-insertPosition+1<=insertPosition-h){
+            j=t;
+            while(j>=insertPosition){
+                Y[j+1]=Y[j];
+                j-=1;
             }
-            Y[i]=x;
-            h=X.length-1;
+            Y[insertPosition]=x;
+            t+=1;
+        }else{
+            if(insertPosition!=0){
+                Y[X.length-1]=Y[0];
+                j=1;
+                while(j<insertPosition){
+                    Y[j-1]=Y[j];
+                    j+=1;
+                }
+                Y[insertPosition-1]=x;
+                h=X.length-1;
             }else{
                 Y[X.length-1]=x;
                 h=X.length-1;
             }
-        }else{
-            for(let j=t;j>i;j--){
-                Y[j+1]=Y[j];
-            }
-            Y[i+1]=x;
-            t+=1;
         }
     }else{
-        let i=t;
-        while(i>=0 && Y[i]>x){
-            i-=1;
+        insertPosition=h;
+        while(insertPosition<X.length && Y[insertPosition]<x){
+            insertPosition+=1;
         }
-        if(i==-1){
-            i=h;
-            while(i<X.length && Y[i]<x){
-                i++;
-            }
-            if(t+X.length-i>=i-h+1){
-                j=h;
-                while(j<i){
-                    Y[j-1]=Y[j];
-                    j++;
-                }
-                Y[i-1]=x;
-                h-=1;
-            }else{
+        if(insertPosition!=X.length){
+            if(X.length-insertPosition+t+1<=insertPosition-h){
                 j=t;
                 while(j>=0){
                     Y[j+1]=Y[j];
                     j-=1;
                 }
                 Y[0]=Y[X.length-1];
-                j=X.length-1;
-                while(j>=i){
-                    Y[j-1]=Y[j];
-                    j-=1;
-                }
-                Y[i]=x;
-                t+=1;
-            }
-        }else{
-            if(t-i<=X.length-h+i+1){
-                j=t;
-                while(j>i){
+                j=X.length-2;
+                while(j>=insertPosition){
                     Y[j+1]=Y[j];
                     j-=1;
                 }
-                Y[i+1]=x;
+                Y[insertPosition]=x;
                 t+=1;
             }else{
                 j=h;
-                while(j<X.length){
+                while(j<insertPosition){
                     Y[j-1]=Y[j];
                     j+=1;
                 }
-                Y[X.length-1]=Y[0];
-                j=0;
-                while(j<i){
-                    Y[j]=Y[j+1];
-                    j+=1;
-                }
-                Y[i]=x;
+                Y[insertPosition-1]=x;
                 h-=1;
             }
+        }else{
+            insertPosition=0;
+            while(insertPosition<=t && Y[insertPosition]<x){
+                insertPosition+=1;
+            }
+            if(t-insertPosition+1<=X.length-h+insertPosition){
+                j=t;
+                while(j>=insertPosition){
+                    Y[j+1]=Y[j];
+                    j-=1;
+                }
+                Y[insertPosition]=x;
+                t+=1;
+            }else{
+                    //check for 0
+                    j=h;
+                    while(j<X.length){
+                        Y[j-1]=Y[j];
+                        j+=1;
+                    }
+                    if(insertPosition!=0){
+                    Y[X.length-1]=Y[0];
+                    j=1;
+                    while(j<insertPosition){
+                        Y[j-1]=Y[j];
+                        j+=1;
+                    }
+                    Y[insertPosition-1]=x;
+                    h-=1;
+                    }else{
+                        Y[X.length-1]=x;
+                        h-=1;
+                    }
+            }
         }
-        
     }
-    console.log(Y);
+    // console.log(Y);
+    // console.log(h);
+    // console.log(t);
 });
 return Y;
-};
+}
+
+// runner([25, 57, 37, 48, 12, 92, 86, 33]);
 
 module.exports = runner;
